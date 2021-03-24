21db8b6139504bae066d9580fbd3a67451602147
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-03-24 10:14:10 +0100

    Build version

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require('express');
-const cors = require('cors');
-const router = express.Router();
+const cors = require('cors')
+const path = require('path');
+const socket = require('socket.io');
+
+const app = express();
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-const app = express();
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use('/api', testimonialsRoutes); 
+app.use('/api', concertsRoutes); 
+app.use('/api', seatsRoutes); 
 
-app.use('/api/', testimonialsRoutes);
-app.use('/api/', concertsRoutes);
-app.use('/api/', seatsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-});
+  res.status(404).send('404 not found...');
+})
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
+const io = socket(server);
 
-module.exports = router;
+io.on('connection', (socket) => {
+  console.log('New socket!');
+});
