405d27f67cf29ffaec834f4a3f2adfd2bd5c0c36
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-03-24 10:24:39 +0100

    Fix typo

---
+++ client/build/precache-manifest.0f10d6210ddb7766d57c614cecc1bb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530662fe6df8d9e3d09ad0908e096697",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edd728041cf3980bd8c",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "09782ca4ea7847236fda",
+    "url": "/static/css/main.4953dd38.chunk.css"
+  },
+  {
+    "revision": "6edd728041cf3980bd8c",
+    "url": "/static/js/2.78961d76.chunk.js"
+  },
+  {
+    "revision": "09782ca4ea7847236fda",
+    "url": "/static/js/main.6225654c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
