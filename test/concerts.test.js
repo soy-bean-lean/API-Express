d6c58416f532409087098e268a97e0154683932d
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-05-04 14:39:37 +0200

    Fix bugs

--- test/concerts.test.js
+++ test/concerts.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const Concert = require('../models/concerts.models');
+const Concerts = require('../models/concerts.models');
 
 chai.use(chaiHttp);
 
@@ -9,21 +9,22 @@ const expect = chai.expect;
 const request = chai.request;
 
 describe('GET /concerts', () => {
+
   before(async () => {
-    const concertOne = new Concert({
+    const concertOne = new Concerts({
       _id: '5d9f1140f10a81216cfd4408',
       performer: 'Marylin Manson',
       genre: 'Metal',
-      price: 35,
+      price: 20,
       day: 1,
     });
     await concertOne.save();
 
-    const concertTwo = new Concert({
+    const concertTwo = new Concerts({
       _id: '5d9f1140f10a81216cfd4409',
       performer: 'Eminem',
       genre: 'Rap',
-      price: 25,
+      price: 30,
       day: 1,
     });
     await concertTwo.save();
@@ -35,37 +36,43 @@ describe('GET /concerts', () => {
     expect(res.body).to.be.an('array');
     expect(res.body.length).to.be.equal(2);
   });
+
   it('should return concert by id', async () => {
     const res = await request(server).get('/api/concerts/5d9f1140f10a81216cfd4408');
     expect(res.status).to.be.equal(200);
     expect(res.body).to.be.an('object');
     expect(res.body.length).to.be.not.be.null;
   });
+
   it('should return performer by name', async () => {
     const res = await request(server).get('/api/concerts/performer/Marylin Manson');
     expect(res.status).to.be.equal(200);
     expect(res.body).to.be.an('array');
     expect(res.body.length).to.be.equal(1);
   });
+
   it('should return genre by name', async () => {
     const res = await request(server).get('/api/concerts/genre/Metal');
     expect(res.status).to.be.equal(200);
     expect(res.body).to.be.an('array');
     expect(res.body.length).to.be.equal(1);
   });
+
   it('should return pricemin/pricemax', async () => {
     const res = await request(server).get('/api/concerts/price/20/30');
     expect(res.status).to.be.equal(200);
     expect(res.body).to.be.an('array');
-    expect(res.body.length).to.be.equal(1);
+    expect(res.body.length).to.be.equal(2);
   });
+
   it('should return concerts by day', async () => {
     const res = await request(server).get('/api/concerts/day/1');
     expect(res.status).to.be.equal(200);
     expect(res.body).to.be.an('array');
     expect(res.body.length).to.be.equal(2);
   });
+
   after(async () => {
-    await Concert.deleteMany();
+    await Concerts.deleteMany();
   });
 });
